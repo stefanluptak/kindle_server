6959c58f4374dca2129afbf29193b34d6268381b
Stefan Luptak <stefan@recycla.se>
2025-03-28 16:19:49 +0100

    Add real data

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM "hexpm/elixir:1.18.3-erlang-27.3-alpine-3.21.3"
 
-RUN apk add --no-cache imagemagick
+RUN apk add --no-cache imagemagick fontconfig ttf-dejavu
 
 RUN wget https://github.com/typst/typst/releases/download/v0.13.1/typst-x86_64-unknown-linux-musl.tar.xz && \
     tar -C /usr/local/bin --strip-components=1 -xf typst-x86_64-unknown-linux-musl.tar.xz && \
@@ -15,4 +15,4 @@ RUN elixir -e 'Mix.install([:tzdata])'
 COPY image.typ.eex .
 COPY script.exs .
 
-ENTRYPOINT ["/bin/sh", "-c", "mkdir -p images && elixir script.exs && typst compile --format png image.typ images/image.png && magick images/image.png -colorspace Gray images/image.png"]
+ENTRYPOINT ["/bin/sh", "-c", "mkdir -p images && elixir script.exs && typst compile --format png --ppi 72 image.typ images/image.png && magick images/image.png -colorspace Gray images/image.png"]
--- script.exs
+++ script.exs
@@ -2,14 +2,74 @@ Mix.install([:tzdata])
 
 Calendar.put_time_zone_database(Tzdata.TimeZoneDatabase)
 
-now = 
+now =
   DateTime.utc_now()
   |> DateTime.shift_zone!("Europe/Bratislava")
   |> DateTime.truncate(:second)
 
+day_of_week_index = String.to_integer(Calendar.strftime(now, "%u"))
+
+day_name =
+  Enum.at(
+    [nil, "PONDELOK", "UTOROK", "STREDA", "ŠTVRTOK", "PIATOK", "SOBOTA", "NEDEĽA"],
+    day_of_week_index
+  )
+
+month_index = String.to_integer(Calendar.strftime(now, "%m"))
+
+month_name =
+  Enum.at(
+    [
+      nil,
+      "JANUÁR",
+      "FEBRUÁR",
+      "MAREC",
+      "APRÍL",
+      "MÁJ",
+      "JÚN",
+      "SEPTEMBER",
+      "OKTÓBER",
+      "NOVEMBER",
+      "DECEMBER"
+    ],
+    month_index
+  )
+
+date = Calendar.strftime(now, "%d. #{month_name} %Y")
+
+{{_year, month, day}, {hour, minute, _second}} =
+  now |> DateTime.to_naive() |> NaiveDateTime.to_erl()
+
+part_of_day =
+  cond do
+    hour < 6 -> "NOC"
+    hour == 6 and minute < 30 -> "NOC"
+    hour < 9 -> "RÁNO"
+    hour < 12 -> "DOOBEDA"
+    hour < 17 -> "POOBEDE"
+    hour < 20 -> "VEČER"
+    true -> "NOC"
+  end
+
+season =
+  cond do
+    month < 3 -> "ZIMA"
+    month == 3 and day < 20 -> "ZIMA"
+    month < 6 -> "JAR"
+    month == 6 and day < 21 -> "JAR"
+    month < 9 -> "LETO"
+    month == 9 and day < 23 -> "LETO"
+    month < 12 -> "JESEŇ"
+    month == 12 and day < 21 -> "JESEŇ"
+    true -> "ZIMA"
+  end
+
 assigns = [
-  name: "Alice",
-  time: Time.to_iso8601(now)
+  day_name: day_name,
+  date: date,
+  part_of_day: part_of_day,
+  season: season,
+  generated_at: Calendar.strftime(now, "%d.%m.%Y o %H:%M")
 ]
 
 typ = EEx.eval_file("image.typ.eex" , assigns: assigns)
